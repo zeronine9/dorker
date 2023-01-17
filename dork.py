c0a12edca39e2c8e06cbd975c89e12ae99324003
Zahir Tariq <60152515+zeronine9@users.noreply.github.com>
2023-01-17 16:56:25 +0200

    Update and rename script1.py to dork.py

--- dork.py
+++ dork.py
@@ -1,10 +1,20 @@
 from googlesearch import *
 
+mylist = [] 
+
 try :
     a = input("Enter the dork here : ")
     b = int(input("Enter Number of web sites you want : "))
 
     for url in search(a,stop=b):
         print(url)
+        mylist.append(url)
 except ValueError:
     print("Pleace Enter a Number ")
+
+zahir = open("results.txt","w")
+for item in mylist:
+    zahir.write(str(item))
+    zahir.write("\n")
+    
+zahir.close()
